e96c158b2e1bdb341af5f4516209ec8ec653d349
Olfnair <chalet.florian@gmail.com>
2017-08-17 16:33:44 +0200

    fix credentials entity

--- src/entities/credentials.ts
+++ src/entities/credentials.ts
@@ -4,15 +4,15 @@
  */
 
 /**
- * Entité Crédentials : login et mot de passe d'un utilisateur
+ * Entité Credentials : login et mot de passe d'un utilisateur
  */
 export class Credentials {
 
   /**
    * @constructor
    * @param {string} login - login de l'utilisateur
-   * @param {string} pwd - mot de passe de l'utilisateur
+   * @param {string} password - mot de passe de l'utilisateur
    */
-  constructor(public login: string, public pwd: string) { }
+  constructor(public login: string, public password: string) { }
 
 }
